433ade081fb99c345ad39be9c023bcf5cd1c22ee
carlosportella16 <carlosportella16@hotmail.com>
2024-12-01 17:13:25 -0300

    fix: terraform lifecycle

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -4,36 +4,13 @@ resource "aws_ecr_repository" "my_api" {
   image_scanning_configuration {
     scan_on_push = true
   }
-}
 
-lifecycle {
-  create_before_destroy = true
-  prevent_destroy       = false
-}
-
-tags = {
-  recreate_trigger = timestamp() # Força recriação com base no timestamp
-}
-
-resource "aws_ecr_lifecycle_policy" "my_api_policy" {
-  repository = aws_ecr_repository.my_api.name
-
-  policy = <<EOF
-  {
-    "rules": [
-      {
-        "rulePriority": 1,
-        "description": "Expire untagged images",
-        "selection": {
-          "tagStatus": "untagged",
-          "countType": "imageCountMoreThan",
-          "countNumber": 10
-        },
-        "action": {
-          "type": "expire"
-        }
-      }
-    ]
+  lifecycle {
+    create_before_destroy = true
+    prevent_destroy       = false
   }
-EOF
+
+  tags = {
+    recreate_trigger = timestamp() # Força recriação com base no timestamp
+  }
 }
