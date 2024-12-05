7f341e63677c294bc7e101d9649937efe5ee7ed8
carlosportella16 <carlosportella16@hotmail.com>
2024-12-05 19:24:35 -0300

    fix: load balancer

--- infra/load_balancer.tf
+++ infra/load_balancer.tf
@@ -41,7 +41,3 @@ resource "aws_lb_listener" "ecs_listener" {
     target_group_arn = aws_lb_target_group.ecs_tg.arn
   }
 }
-
-lifecycle {
-  prevent_destroy = true
-}
--- infra/logs.tf
+++ infra/logs.tf
@@ -1,7 +1,8 @@
 resource "aws_cloudwatch_log_group" "ecs_logs" {
   name              = "/ecs/my-container-logs"
   retention_in_days = 7
-}
-lifecycle {
-  prevent_destroy = true
+
+  tags = {
+    Name = "ecs-container-logs"
+  }
 }
