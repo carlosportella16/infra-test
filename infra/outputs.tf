f7705eb05b26c7c552b9e889c59b8b198f695f6a
carlosportella16 <carlosportella16@hotmail.com>
2024-12-06 17:45:41 -0300

    chore: outputs.infra

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,19 +1,7 @@
-output "ecr_repository_url" {
-  value = aws_ecr_repository.my_api.repository_url
+output "alb_sg_id" {
+  value = aws_security_group.alb_sg.id
 }
 
-output "ecs_cluster_name" {
-  value = aws_ecs_cluster.my_cluster.name
-}
-
-output "vpc_id" {
-  value = aws_vpc.my_vpc.id
-}
-
-output "subnet_ids" {
-  value = aws_subnet.my_subnets[*].id
-}
-
-output "security_group_id" {
-  value = aws_security_group.my_sg.id
+output "service_sg_id" {
+  value = aws_security_group.ecs_service_sg.id
 }
