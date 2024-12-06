5fafbef05393dee6e9c28450fb4c12717bb58190
carlosportella16 <carlosportella16@hotmail.com>
2024-12-06 18:04:54 -0300

    fix: alb target group

--- infra/modules/alb/main.tf
+++ infra/modules/alb/main.tf
@@ -14,6 +14,8 @@ resource "aws_lb_target_group" "this" {
   port     = var.target_port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  target_type = "ip"
+
   health_check {
     path = var.health_check_path
   }
